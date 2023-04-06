cd717ecf825f667cffefe3bd3c5234c54ff42e18
Rajeev verma <rajeevverma@Rajeevs-MacBook-Air.local>
2023-04-06 21:46:30 +0530

    added eslint or prettier

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@chakra-ui/react";
+
+export const Home = () => {
+  return (
+    <div>
+      home
+      <Button colorScheme="green">Done</Button>
+    </div>
+  );
+};
